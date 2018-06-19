120cac4888dc8a73b72686eddfa71ad1e9167bb8
Rodrigo Giraldi Silva <rgiraldisilva@gmail.com>
2018-06-19 14:09:35 -0400

    new-sprint initial form created

--- src/app/new-sprint/new-sprint.component.ts
+++ src/app/new-sprint/new-sprint.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SprintTemplate } from '../sprint-template';
+
 @Component({
   selector: 'app-new-sprint',
   templateUrl: './new-sprint.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewSprintComponent implements OnInit {
 
+  sprintTemplates: SprintTemplate[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.sprintTemplates = [
+      {name: 'Instant', duration: 5, status: ""},
+      {name: 'Very Short', duration: 300, status: ""},
+      {name: 'Short', duration: 600, status: ""},
+      {name: 'Pomodoro', duration: 1500, status: ""},
+      {name: 'Long', duration: 2700, status: ""},
+      {name: 'Very Long', duration: 3600, status: ""}
+    ]
   }
 
 }
---
+++ src/app/sprint-template.ts
@@ -0,0 +1,5 @@
+export class SprintTemplate{
+    name: string;
+    duration: number;
+    status: string;
+}
