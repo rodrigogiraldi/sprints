731f1fecc2e0d9c54683286d29757f6b07c2d777
Rodrigo Giraldi Silva <rgiraldisilva@gmail.com>
2018-06-22 16:19:48 -0400

    added desktop notification after complete sprint

--- services/past-sprint.service.js
+++ services/past-sprint.service.js
@@ -1,6 +1,15 @@
+const notifier = require('node-notifier');
+
 const pastSprintDAO = require('../daos/past-sprint.dao');
 
 exports.createSprint = async function (pastSprint) {
+
+    if (pastSprint.notify) {
+        notifier.notify({
+            title: 'Sprints App',
+            message: 'Sprint completed!'
+        });
+    }
 
     try {
         const pastSprintCreated = await pastSprintDAO.create(pastSprint);
