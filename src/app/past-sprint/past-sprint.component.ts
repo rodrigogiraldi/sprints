13f7dcc6364712c8648e9e5c63dbe9a7c1a5cd78
Giraldi Silva <rodrigo.giraldisilva@cgi.com>
2018-06-26 13:46:30 -0400

    implemented items per page toolbar
    -selecting number of items per page
    --only toolbar, not filtering yet
    -showing pagination toolbar only if necessary

--- src/app/past-sprint/past-sprint.component.ts
+++ src/app/past-sprint/past-sprint.component.ts
@@ -10,6 +10,14 @@ import { PastSprintService } from '../past-sprint.service';
 export class PastSprintComponent implements OnInit {
 
   pastSprints: PastSprint[];
+  pastSprintscurrentPage: PastSprint[] = [];
+  itemsPerPageElements: any[] = [
+    { value: 5, css: { active: false } },
+    { value: 10, css: { active: true } },
+    { value: 25, css: { active: false } }
+  ];
+  currentPage: number = 0;
+  needsPagination: boolean = true;
 
   constructor(private pastSprintService: PastSprintService) { }
 
@@ -21,6 +29,35 @@ export class PastSprintComponent implements OnInit {
     this.pastSprintService.getSprints()
       .subscribe(res => {
         this.pastSprints = res.data;
+        this.pastSprintscurrentPage = this.pastSprints;
       })
+  }
+
+  changeItemsPerPage(itemText) {
+    this.itemsPerPageElements = this.itemsPerPageElements.map((current, index, arr) => {
+      if (current.value == itemText) {
+        current.css.active = true;
+
+      } else {
+        current.css.active = false;
+      }
+      return current;
+    });
+
+    this.checkIfNeedsPagination();
+  }
+
+  getActive(): any {
+    return this.itemsPerPageElements.find(el => { return el.css.active == true });
+  }
+
+  checkIfNeedsPagination() {
+    let activeElement = this.getActive();
+    if (this.pastSprintscurrentPage.length > activeElement.value) {
+      this.needsPagination = true;
+    }
+    else {
+      this.needsPagination = false;
+    }
   }
 }
